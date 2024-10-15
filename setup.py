fb17a39bcaedba5cc210691bd28fd85655cc3c34
Enes Esvet Kuzucu <eneskuzucu@hotmail.com>
2024-10-15 16:21:18 +0300

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name='string2dict',  # Package name
-    version='0.0.4',  # Version of your package
+    version='0.0.5',  # Version of your package
     author='Enes Kuzucu',  # Your name
 
     description='easily convert json like strings to dictionaries',  # Short description
